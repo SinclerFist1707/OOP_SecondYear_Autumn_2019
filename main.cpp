fa4b83d88cebe06ba205adf84ba09771c51b9750
SinclerFist1707 <58069425+SinclerFist1707@users.noreply.github.com>
2019-12-20 12:17:54 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	double a1,b1,a2,b2,a,b,c,l1;
+	double p=3.141592653589,l=40000;
+	cin>>a1>>b1>>a2>>b2;
+	a=abs(a1-a2); b=abs(b1-b2);
+		c=((2-pow((sin(p*a/180)/sin(p*(90-a/2)/180)),2)-pow((sin(p*b/180)/sin(p*(90-b/2)/180)),2))/2);
+	if(c>1)
+		while(c>1)
+			c-=2;
+	if(c==1)
+		c-=0.0000000000001;
+	if(c==-1)
+		c+=0.0000000000001;;
+	if(c<-1)
+		while(c<-1)
+			c+=2;
+	l1=acos(c)/p/2*l;
+	cout<<l1;
+	system("pause");
+	return 0;
+}
